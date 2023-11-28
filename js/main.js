8679eb7aa918217be28c68ed64007e84b956391f
vbouq <v.bouquet707@gmail.com>
2023-11-28 12:54:03 -0800

    "second commit, more work on main.js"

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@
 
 
 /*------ constants ------*/
+// our audio file to play during countdown
+const AUDIO = new AUDIO()
 //list of choices
 const RPS_LOOKUP = {
     r: {img: 'imgs/rock.png', beats: 's'},
@@ -38,5 +40,101 @@ let results
 let winner
 
 /*------ functions ------*/
+// initializer function --> set up our initial state and call render()
+function init() {
+    scores = {
+        p: 0,
+        c: 0,
+        t: 0,
+    }
+    results = {
+        p: 'r',
+        c: 'r',
+    }
+    winner = 't'
+
+    //eventually call render when render has been built
+    render()
+}
+
+init()
+
+//renderScores -> show how many wins, losses and ties
+function renderScores() {
+    //loop over the scores object, display scores accordingly
+    for (let key in scores) {
+        //we're using key to select an html element
+        const scoreEl = document.getElementById(`${key}-score`)
+        //we're using bracket notation to dynamically use object values based on a changing key
+        scoreEl.innerText = scores[key]
+    }
+}
+//renderResults -> show the results of player and computer choices
+function renderResults() {
+    //this looks at the results object, pulls the values for the keys P and C
+    //then applies the image from the related part of the RPS_LOOKUP object
+    //as the src for the img tag associated with the player and computer
+    pResultEl.src = RPS_LOOKUP[results.p].img
+    cResultEl.src = RPS_LOOKUP[results.c].img
+    //this will visually identify the winner
+    //if this el belongs to the winner, change to purple
+    //otherwise use white
+    pResultEl.style.borderColor = winner === 'p' ? 'purple' : 'white'
+    cResultEl.style.borderColor = winner === 'c' ? 'purple' : 'white'
+}
+
+//render countdown -> this will play audio and display the countdown to the user
+//this will use a callback function
+function renderCountdown(cbFunc){
+    //start the count at 3
+    let count = 3;
+    //display the countdown div and set the text
+    countdownEl.style.visibility = 'visible';
+    countdownEl.innerText = count
+
+    //timer will update the DOM every second
+    //once the timer is up, display the results
+    AUDIO.currentTime = 0;
+    AUDIO.play()
+
+    //set up timer
+    //setInterval takes two arguments -> callback fn, time(in ms)
+    const timerId = setInterval(() => {
+        //every second, decrease the count
+        count--
+        if (count) {
+            //if count is truthy, do something
+            console.log('interval running. count : ', count)
+            countdownEl.innerText = count
+        } else {
+            //otherwise, do something else
+            clearInterval(timerId)
+            //once timer is done, hide it
+            countdownEl.style.visibility = 'hidden'
+
+            //when the timer is done, call the callback function
+            cbFunc()
+        }
+        //timeouts and intervals use milliseconds
+    }, 1000)
+
+}
+
+//render -> transfer/visualize all changes through the dom
+function render() {
+    renderCountdown(() => {
+        renderScores()
+        renderResults()
+    })
+}
+
+//we'll do this by calling a couple other render functions through a countdown
+
+
+//getRandom function for our comp player to select a move
+
+//handleChoice -> for the player to select a move(this will be an event listener)
+
+//need a getWinner function -> determine who wins - player, comp, or a tie
 
 /*------ event listeners ------*/
